8cd2b022489aab913d5a877c17d4bbb8781ebee9
Khusrav Khamidov <khusravkhamidov@gmail.com>
2022-06-21 01:57:47 +0600

    Initial Commit

---
+++ netflix2/netflix2App.swift
@@ -0,0 +1,17 @@
+//
+//  netflix2App.swift
+//  netflix2
+//
+//  Created by yaaburnee on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct netflix2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
