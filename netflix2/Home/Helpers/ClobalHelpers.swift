d6e8c3f08e3f4869068cf140b74a4b84b444081b
flawloop <khusravkhamidov@gmail.com>
2022-08-11 05:51:23 +0600

    Added a custom Search Bar with full functionality

--- netflix2/Home/Helpers/ClobalHelpers.swift
+++ netflix2/Home/Helpers/ClobalHelpers.swift
@@ -191,3 +191,9 @@ extension String {
         return size.width
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
---
+++ netflix2/Home/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  netflix2
+//
+//  Created by yaaburnee on 11/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}
